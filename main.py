ca798c1dfdebee7a93d2d148b87e00b441ab55b7
Ernesto David Peña Herrera <ernestodavidph@gmail.com>
2023-05-08 14:34:00 +0200

    wip

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from manager.setup_models import setup_models
-from manager.populate_tables import populate_tables
+from manager.populate_tables import populate_tables, drop_tables
 
 if __name__ == "__main__":
     # create tables
