98e71aa5dd1d1855e61095701fbb6d96637853c2
Ernesto David Peña Herrera <ernestodavidph@gmail.com>
2023-05-08 14:21:06 +0200

    fix bug and add PurchaseDate field to purchases

--- models/purchases.py
+++ models/purchases.py
@@ -1,10 +1,11 @@
-from manager.models_type import DBForeignKey
+from manager.models_type import DBForeignKey, DBDate
 from manager.models_type import table_keys
 
-from models.author import Author
+from models.user import User
 from models.book import Book
 
 @table_keys('UserId', 'BookId')
 class Purchases:
-    UserId  = DBForeignKey(Author, "AuthorId")
+    UserId  = DBForeignKey(User, "UserId")
     BookId    = DBForeignKey(Book, "BookId")
+    PurchaseDate = DBDate()
