98a53d9fe7ac03fcba724511c758b6437d076faa
Yordan Laborde <yordanluislp@gmail.com>
2023-05-09 00:48:26 +0200

    queries created

--- services/purchases_service.py
+++ services/purchases_service.py
@@ -1,4 +1,5 @@
 from manager.connection import connect, SCHEMA_NAME
+import datetime
 
 def get_purchases_count(guery_data):
     con = connect()
@@ -18,3 +19,42 @@ def get_purchases_by_user(guery_data):
     sol = con.fetchall()
 
     return sol
+
+def create_purchase(query_data):
+    con = connect()
+    con.execute("set TRANSACTION ISOLATION LEVEL SERIALIZABLE;")
+    con.execute("BEGIN")
+
+    con.execute(f"""select * from {SCHEMA_NAME}.user u
+                    join {SCHEMA_NAME}.userprofile p on u.userid = p.userid
+                    where u.userid = {query_data['UserId']}""")
+    
+    user_data = con.fetchone()
+    if user_data == None:
+        return "The UserId provided was not found, please provide a valid one"
+    
+    con.execute(f"select * from {SCHEMA_NAME}.book where bookid = {query_data['BookId']}")
+    book_data = con.fetchone()
+    if book_data == None:
+        return "The BookId provided was not found, please provide a valid one"
+
+    user_balance = float(user_data[5].replace("$", ""))
+    book_price = float(book_data[4].replace("$", ""))
+
+    print(user_balance, book_price)
+    if user_balance < book_price:
+        return "The user doesn't have enough balance to purchase the book"
+
+
+    now = datetime.datetime.now()
+    con.execute(f"""insert into {SCHEMA_NAME}.purchases (UserId, BookId, PurchaseDate) 
+                values('{query_data['UserId']}', '{query_data['BookId']}', '{now}')""")
+    
+    con.execute(f"""update {SCHEMA_NAME}.userprofile
+                    set balance = balance - {book_price}::money
+                    where userid = {query_data['UserId']}
+    """)
+
+    con.execute("commit")
+    
+    return "Purchase created successfully"
