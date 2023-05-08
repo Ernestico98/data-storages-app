e47c0e14f1c44da8ccf273d95b474b11053577bf
Mariano Jason <mjasonrc@gmail.com>
2023-05-08 15:33:14 +0200

    Cleaned some functions

--- manager/populate_tables.py
+++ manager/populate_tables.py
@@ -9,27 +9,6 @@ from manager.connection import connect, SCHEMA_NAME
 from faker import Faker
 
 
-# def populate_tables():
-#     # First stage, load data from jsons
-#     for model in MODELS:
-#         json_name = os.path.join("data", model.__name__.lower() + ".json")
-        
-#         if os.path.isfile(json_name):
-#             with open(json_name, "r") as file:
-#                 content = file.read()
-#                 content = json.loads(content)
-
-#                 # list of objects
-#                 if type(content) == list:
-#                     for pack in content:
-#                         add_from_dict(pack, model.__name__)
-#                 # only one object
-#                 else:
-#                     add_from_dict(content, model.__name__)
-
-#     # Second stage, generate some data randomly
-
-
 def populate_tables():
     faker = Faker()
 
