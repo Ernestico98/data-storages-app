15c19779bad19abd73b0940d4fbdc537ba1d43e7
Mariano Jason <mjasonrc@gmail.com>
2023-05-16 18:48:44 +0200

    Fixed ttl and cart with redis stats

--- README.md
+++ README.md
@@ -71,7 +71,7 @@ for more information use --help
 ### Managing high RPS using redis
 #### Scenario 1: "Creation of shoping cart"
 
-There were 5 methods (add_to_cart, remove_from_cart, clear_cart, purchase_from_cart, get_cart_contents) implemented to support this functionality. Basically a unique key ("{SCHEMA}\_shop_cart\_{UserId}") is assigned to each user to store a set of ids of books in the user's cart. Also each key has a TTL of 60 minutes and is refreshed each time an operation with this key is made.
+There were 5 methods (add_to_cart, remove_from_cart, clear_cart, purchase_from_cart, get_cart_contents) implemented to support this functionality. Basically a unique key ("{SCHEMA}\_shop_cart\_{UserId}") is assigned to each user to store a set of ids of books in the user's cart.
 
 
 In addition, a general statistic of the status of book sales can be obtained with the method:
--- services/purchases_service.py
+++ services/purchases_service.py
@@ -1,4 +1,4 @@
-from manager.connection import connect, SCHEMA_NAME, connect_rc, TTL_IN_SECONDS
+from manager.connection import connect, SCHEMA_NAME, connect_rc
 import datetime
 from prettytable import PrettyTable
 import json
@@ -56,7 +56,7 @@ def add_to_cart(query_data):
         # fast stats 
         value_key = f"{SCHEMA_NAME}_stbookcart_{query_data['BookId']}"
         current_value = rc.get(value_key)
-        current_value = 0 if current_value is None else current_value
+        current_value = 0 if current_value is None else int(current_value)
         current_value += 1
         rc.set(value_key, current_value)
         
@@ -64,7 +64,6 @@ def add_to_cart(query_data):
         error_message = str(e)
         return f"Some error has occurred. Error message: {error_message}"
     
-    rc.expire(cart_key, TTL_IN_SECONDS)
     return "Book added successfully to the cart"
 
 def remove_from_cart(query_data):
@@ -77,7 +76,7 @@ def remove_from_cart(query_data):
         # fast stats 
         value_key = f"{SCHEMA_NAME}_stbookcart_{query_data['BookId']}"
         current_value = rc.get(value_key)
-        current_value = 0 if current_value is None else current_value
+        current_value = 0 if current_value is None else int(current_value)
         current_value = max(0, current_value-1)
         rc.set(value_key, current_value)
 
@@ -85,7 +84,6 @@ def remove_from_cart(query_data):
         error_message = str(e)
         return f"Some error has occurred. Error message: {error_message}"
     
-    rc.expire(cart_key, TTL_IN_SECONDS)
     return "Book removed successfully"
 
 def purchase_from_cart(query_data):
@@ -103,7 +101,6 @@ def get_cart_contents(query_data):
     cart_key = f"{SCHEMA_NAME}_shop_cart_{query_data['UserId']}"
     cart_items = rc.smembers(cart_key)
     cart_ids = [int(item) for item in cart_items]
-    rc.expire(cart_key, TTL_IN_SECONDS)
     return cart_ids
 
 def clear_cart(query_data):
@@ -115,11 +112,8 @@ def clear_cart(query_data):
         # fast stats 
         cart_items = rc.smembers(cart_key)
         for item in cart_items:    
-            value_key = f"{SCHEMA_NAME}_stbookcart_{item}"
-            current_value = rc.get(value_key)
-            current_value = 0 if current_value is None else current_value
-            current_value = max(0, current_value-1)
-            rc.set(value_key, current_value)
+            value_key = f"{SCHEMA_NAME}_stbookcart_{int(item)}"
+            rc.delete(value_key)
 
         rc.delete(cart_key)
     except Exception as e:
@@ -197,9 +191,9 @@ def create_purchase(query_data):
 
         # fast stats 
         for bid in query_data['BooksIds']:
-            value_key = f"{SCHEMA_NAME}_stbookbuy_{bid}"
+            value_key = f"{SCHEMA_NAME}_stbookbuy_{int(bid)}"
             current_value = rc.get(value_key)
-            current_value = 0 if current_value is None else current_value
+            current_value = 0 if current_value is None else int(current_value)
             current_value += 1
             rc.set(value_key, current_value)
 
